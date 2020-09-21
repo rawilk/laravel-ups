d5b98101cfe1d01e4b42a6edd6a8d73615cadb0c
Randall Wilk <wilk.randall@gmail.com>
2020-09-21 07:33:30 -0500

    Make TestCase abstract

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,7 @@ namespace Rawilk\Ups\Tests;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Rawilk\Ups\UpsServiceProvider;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
     protected function getPackageProviders($app): array
     {
